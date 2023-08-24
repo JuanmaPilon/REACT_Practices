35a2ea8dcd8833fb4ff2d847e665a2bf5cc0fdb1
Juanma <juanmartin.pilon@gmail.com>
2023-08-24 12:59:50 -0300

    Dejando funcionando el contador de taras, me falta agregarle que se reflejen las tareas y que pueda piniearlas en una lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import React, { useState } from "react";
 import Saludo from "./Saludo";
 import ListaTareas from "./ListaTareas"
 import Contador from "./Contador";
 
 function App() {
+    const [cantidadTareas, setCantidadTareas] = useState(0);
+
     return (
         <>
         <Saludo name = 'Pepito' />
         <Saludo name = 'Juanito' />
-        <ListaTareas />
-        <Contador />
+        <ListaTareas cantidadTareas={cantidadTareas} setCantidadTareas={setCantidadTareas} />
+        <Contador cantidadTareas={cantidadTareas}/>
         </>
     );
 }
--- src/ListaTareas.jsx
+++ src/ListaTareas.jsx
@@ -1,10 +1,17 @@
-function ListaTareas() {
+import React from "react";
+
+function ListaTareas({cantidadTareas, setCantidadTareas}) {
     const tareas = ['Hacer compras', 'Estudiar Programacion', 'Salvar el semestre', 'Hacer ejercicio'];
 
+    const agregarTarea = () => {
+        setCantidadTareas(cantidadTareas + 1);
+    };
 
 return (
     <div>
         <h1>Lista de las tareas</h1>
+        <h2>Cantidad de tareas actualmente: {cantidadTareas}</h2>
+        <button onClick={agregarTarea}>Agrega una tarea!</button>
         <ul>
             {tareas.map((tarea, index) => (<li key={index}>{tarea}</li>))}
         </ul>
