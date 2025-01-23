57e7e47cf7840a19f0251364f6df61078fe3aa09
JuanmaPilon <juanmartin.pilon@gmail.com>
2025-01-23 12:48:02 -0300

    Adding a little bit more of the structure

---
+++ WoW Project/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+function navBar() {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <h1>Navbar</h1>
+    </nav>
+  );
+}
+
+export default navBar;
--- WoW Project/src/main.jsx
+++ WoW Project/src/main.jsx
@@ -1,12 +1,10 @@
-import './index.css'
-// @deno-types="@types/react"
-import { StrictMode } from 'react'
-// @deno-types="@types/react-dom/client"
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import "./css/index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
