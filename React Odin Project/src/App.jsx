262ac89cf21052296268b8e3dca076c68098b6cb
JuanmaPilon <juanmartin.pilon@gmail.com>
2025-01-28 11:49:57 -0300

    Adding the base for the odin project course

---
+++ React Odin Project/src/App.jsx
@@ -0,0 +1,14 @@
+import "./css/App.css";
+import Greetings from "./components/Greetings.jsx";
+
+
+function App() {
+  return (
+    <>
+      <h1>Odin Project</h1>
+      <Greetings/>
+    </>
+  );
+}
+
+export default App;
