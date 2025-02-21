37d4817fa11bde967529a2e04f37409868943cfd
JuanmaPilon <juanmartin.pilon@gmail.com>
2025-02-21 15:05:39 -0300

    trying some stuff, nothing in particular xd

--- react-ts-next/src/app/page.tsx
+++ react-ts-next/src/app/page.tsx
@@ -1,7 +1,34 @@
+type Color = "red" | "blue" | "green";
+
+
+type ButtonProps = {
+  text: string;
+  subtitle?: string;
+  color?: string;
+  background?: Color;
+}
+
+function Button({text, color, subtitle} : ButtonProps) {
+
+
+
+
+  return (
+    <button>
+   {text}
+    </button>
+  )
+}
+
+
 function Page() {
   return (
     <div>
-      <h1>Page</h1>
+      <Button 
+      text="Alo"
+      background="red"
+      color="blue"
+      />
     </div>
   )
 }
